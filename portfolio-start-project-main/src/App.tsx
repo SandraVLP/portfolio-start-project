bb084058d20b9367aa83a921c7a819789513ca87
SandraVLP <aleksanvp@yandex.ru>
2024-02-02 20:59:19 +0300

    структура

--- portfolio-start-project-main/src/App.tsx
+++ portfolio-start-project-main/src/App.tsx
@@ -1,11 +1,27 @@
 import './App.css';
 import styled from 'styled-components';
+import { Header } from './layout/Header';
+import { Main } from './layout/sections/main/Main';
+import { About } from './layout/sections/about/About';
+import { Skills } from './layout/sections/skills/Skills';
+import { Services } from './layout/services/Services';
+import { Portfolio } from './layout/portfolio/Portfolio';
+import { Contacts } from './layout/contacts/Contacts';
+import { Footer } from './layout/Footer';
 
 
 function App() {
     return (
+        
         <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
+            <Header />
+            <Main />
+            <About/>
+            <Skills/>
+            <Services/>
+            <Portfolio/>
+            <Contacts/>
+            <Footer/>
         </div>
     );
 }
