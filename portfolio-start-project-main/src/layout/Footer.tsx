a769b7307b2ac29401152c250943257372d3f3c0
SandraVLP <aleksanvp@yandex.ru>
2024-02-03 21:00:49 +0300

    footer

--- portfolio-start-project-main/src/layout/Footer.tsx
+++ portfolio-start-project-main/src/layout/Footer.tsx
@@ -1,8 +1,23 @@
+import styled from "styled-components";
+import { Icon } from "../components/Icon";
 
 export function Footer() {
     return (
-<footer>
+<StyledFooter>
 <span>Designed and developed by Rostam Sadiqi 2021</span>
-</footer>
+<div className="icon-container">
+    <Icon iconId="ball"></Icon>
+    <Icon iconId="be"></Icon>
+    <Icon iconId="in"></Icon>
+    <Icon iconId="inst"></Icon>
+</div>
+
+</StyledFooter>
     );
 }
+
+const StyledFooter = styled.footer `
+    background-color: rgba(15, 191, 97, 0.1);
+    display: flex;
+    justify-content: space-between;
+`
