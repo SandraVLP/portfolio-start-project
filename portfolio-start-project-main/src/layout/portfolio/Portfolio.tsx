3e1f1549cd04eb99d0bbf3685dad0ff082aab958
SandraVLP <aleksanvp@yandex.ru>
2024-02-03 19:56:06 +0300

    changes of images

--- portfolio-start-project-main/src/layout/portfolio/Portfolio.tsx
+++ portfolio-start-project-main/src/layout/portfolio/Portfolio.tsx
@@ -1,8 +1,19 @@
 
 export function Portfolio() {
     return (
-<section>
+<section className="Portfolio">
 <h2>My recent works</h2>
+<ul>
+    <li>All</li>
+    <li>UX/UI Design</li>
+    <li>Web Design</li>
+    <li>Front End Development</li>
+</ul>
+<div className="portfolio-container">
+    <img></img>
+</div>
+<button>See more</button>
+
 </section>
     );
 }
