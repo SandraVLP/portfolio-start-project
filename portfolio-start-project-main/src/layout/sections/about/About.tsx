828cfea2d2229f6dd132a3944e2919805e4759cd
SandraVLP <aleksanvp@yandex.ru>
2024-02-03 15:05:22 +0300

    About

--- portfolio-start-project-main/src/layout/sections/about/About.tsx
+++ portfolio-start-project-main/src/layout/sections/about/About.tsx
@@ -1,6 +1,12 @@
+import styled from "styled-components";
+import FotoAbout from "../../../assets//images/foto-about.jpg"
+
 export function About() {
   return (
-    <section>
+    <StyledAbout className="About">
+     
+      <StyledImg src={FotoAbout} alt="Foto about"></StyledImg>
+      <StyledContainer className="text-container">
       <h2>About me</h2>
       <p>
         Hello! My name is Rostam Sadiqi and I am a UX/UI designer and Front End
@@ -11,6 +17,26 @@ export function About() {
         engineering, creating dynamic and beautiful web pages. I have been in
         the field for nearly 5 years, and have been loving every minute of it.{" "}
       </p>
-    </section>
+      </StyledContainer>
+    </StyledAbout>
   );
 }
+
+const StyledAbout = styled.section `
+  display: flex;
+  justify-content: space-around;
+  padding-top: 140px;
+  padding-bottom: 140px;
+`
+
+const StyledImg = styled.img `
+  max-width: 280px;
+  border-radius: 20px;
+`
+
+const StyledContainer = styled.div `
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  max-width: 550px;
+`
