1ffda482414aea38178b2618c8551981e3b46e40
SandraVLP <aleksanvp@yandex.ru>
2024-02-03 14:13:08 +0300

    header

--- portfolio-start-project-main/src/layout/Header.tsx
+++ portfolio-start-project-main/src/layout/Header.tsx
@@ -1,14 +1,73 @@
+import styled from "styled-components";
+import logo from "../assets/images/Logo.svg";
 
 export function Header() {
-    return (
-<header>
-    <nav><ul>
-        <li><a href=""></a>Home</li>
-        <li><a href=""></a>About</li>
-        <li><a href=""></a>Service</li>
-        <li><a href=""></a>My work</li>
-        <li><a href=""></a>Resume</li>
-        </ul></nav>
-</header>
-    );
+  return (
+    <StyledHeader>
+        <img src={logo} alt="logo"></img>
+      <StyledNav>
+        
+        <StyledList>
+          <StyledLi>
+            <StyledA href="">Home</StyledA>
+          </StyledLi>
+          <StyledLi>
+            <StyledA href="">About</StyledA>
+          </StyledLi>
+          <StyledLi>
+            <StyledA href="">Service</StyledA>
+          </StyledLi>
+          <StyledLi>
+            <StyledA href="">My work</StyledA>
+          </StyledLi>
+          <StyledLi>
+            <StyledA href="">Resume</StyledA>
+          </StyledLi>
+        </StyledList>
+      </StyledNav>
+    </StyledHeader>
+  );
 }
+
+const StyledHeader = styled.header`
+    background-color: rgba(15, 191, 97, 0.1);
+    display: flex;
+    justify-content: space-around;
+
+    
+;
+`
+
+const StyledNav = styled.nav`
+        display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const StyledList = styled.ul`
+    display: flex;
+    justify-content: space-between;
+    list-style-type: none;
+    margin: 0 auto;
+    padding: 0;  
+`
+
+const StyledA = styled.a`
+    text-decoration: none;
+color: rgba(0, 0, 0, 1);
+
+`
+
+const StyledLi = styled.li`
+margin-right: 30px;
+&:first-child {
+color:rgba(15, 191, 97, 1);}
+&:last-child {
+    background: rgba(15, 191, 97, 1);
+    width: 146px;
+    text-align: center;
+    border-radius: 50px;
+   margin-right: 0;
+  }
+
+`
