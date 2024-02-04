2538b1df5e3937b5a4ee79be66c01394c77e3c06
SandraVLP <aleksanvp@yandex.ru>
2024-02-04 14:08:27 +0300

    import change

--- portfolio-start-project-main/src/App.tsx
+++ portfolio-start-project-main/src/App.tsx
@@ -1,33 +1,26 @@
-import './App.css';
-import styled, { ThemeProvider } from 'styled-components';
-import { Header } from './layout/Header';
-import { Main } from './layout/sections/main/Main';
-import { About } from './layout/sections/about/About';
-import { Skills } from './layout/sections/skills/Skills';
-import { Services } from './layout/services/Services';
-import { Portfolio } from './layout/portfolio/Portfolio';
-import { Contacts } from './layout/contacts/Contacts';
-import { Footer } from './layout/Footer';
-import { myTheme } from './styles/Theme';
-
+import "./App.css";
+import { Header } from "./layout/Header";
+import { Main } from "./layout/sections/main/Main";
+import { About } from "./layout/sections/about/About";
+import { Skills } from "./layout/sections/skills/Skills";
+import { Services } from "./layout/services/Services";
+import { Portfolio } from "./layout/portfolio/Portfolio";
+import { Contacts } from "./layout/contacts/Contacts";
+import { Footer } from "./layout/Footer";
 
 function App() {
-    return (
-        
-        <div className="App">
-            <ThemeProvider theme={myTheme}>
-            <Header />
-            <Main />
-            <About/>
-            <Skills/>
-            <Services/>
-            <Portfolio/>
-            <Contacts/>
-            <Footer/>
-            </ThemeProvider>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <About />
+      <Skills />
+      <Services />
+      <Portfolio />
+      <Contacts />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
-
--- portfolio-start-project-main/src/index.tsx
+++ portfolio-start-project-main/src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyles } from './styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import { myTheme } from './styles/Theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={myTheme}>
     <App />
-    <GlobalStyles></GlobalStyles>
+    <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
