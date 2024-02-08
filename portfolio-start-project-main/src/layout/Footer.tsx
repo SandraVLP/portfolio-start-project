aa74eac96486506d73a2745ea5ef5f14f8c95438
SandraVLP <aleksanvp@yandex.ru>
2024-02-08 14:16:11 +0300

    container in footer

--- portfolio-start-project-main/src/layout/Footer.tsx
+++ portfolio-start-project-main/src/layout/Footer.tsx
@@ -2,38 +2,44 @@ import styled from "styled-components";
 import { Icon } from "../components/Icon";
 import { myTheme } from "../styles/Theme";
 import { FlexWrapper } from "../components/FlexWrapper";
+import { Container } from "../components/Container";
+// import Tele from "../assets/images/telegram-svgrepo-com.svg";
 
 export function Footer() {
   return (
     <StyledFooter>
-      <small>Designed and developed by Rostam Sadiqi 2021</small>
-      
-        <FlexWrapper gap="20px" width="fit-content">
-          <Icon
-            iconId="teleg"
-            width="22px"
-            height="22px"
-            viewbox="0 0 22 22"
-          ></Icon>
-          <Icon
-            iconId="be"
-            width="22px"
-            height="22px"
-            viewbox="0 0 22 22"
-          ></Icon>
-          <Icon
-            iconId="in"
-            width="22px"
-            height="22px"
-            viewbox="0 0 22 22"
-          ></Icon>
-          <Icon
-            iconId="inst"
-            width="22px"
-            height="22px"
-            viewbox="0 0 22 22"
-          ></Icon>
+      <Container>
+        <FlexWrapper>
+          <small>Designed and developed by Rostam Sadiqi 2021</small>
+          <FlexWrapper gap="20px" width="fit-content">
+            <Icon
+              iconId="be"
+              width="22px"
+              height="22px"
+              viewbox="0 0 22 22"
+            ></Icon>
+            <Icon
+              iconId="tele"
+              width="22px"
+              height="22px"
+              viewbox="0 0 22 22"
+            ></Icon>
+            {/* <img src={Tele}></img> */}
+            <Icon
+              iconId="in"
+              width="22px"
+              height="22px"
+              viewbox="0 0 22 22"
+            ></Icon>
+            <Icon
+              iconId="inst"
+              width="22px"
+              height="22px"
+              viewbox="0 0 22 22"
+            ></Icon>
+          </FlexWrapper>
         </FlexWrapper>
+      </Container>
     </StyledFooter>
   );
 }
@@ -41,9 +47,5 @@ export function Footer() {
 const StyledFooter = styled.footer`
   height: 10vh;
   background-color: ${myTheme.secondary};
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-
   
 `;
