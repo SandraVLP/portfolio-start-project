1bed31e5ad3a86fe6f5d7d38e69469c8b2577de4
SandraVLP <aleksanvp@yandex.ru>
2024-02-11 18:24:12 +0300

    service adaptiv

--- portfolio-start-project-main/src/layout/services/service/Service.tsx
+++ portfolio-start-project-main/src/layout/services/service/Service.tsx
@@ -12,8 +12,8 @@ type ServicePropsType = {
 
 export const Service = (props: ServicePropsType) => {
   return (
-    <StyledDescription className="service-description">
-      <ServiceInfo className="service-info">
+    <StyledDescription>
+      <ServiceInfo>
         <p>{props.title}</p>
         <StyledTextDescription>{props.description}</StyledTextDescription>
         <a href={props.link}>
@@ -35,13 +35,25 @@ const StyledDescription = styled.div`
   margin-bottom: 80px;
   height: 400px;
   display: flex;
-  flex-wrap: wrap-reverse;
+  gap:80px;
 
-  div {
-    max-width: 380px;
+  @media ${myTheme.media.tablet} {
+    padding: 30px;
+    gap:30px;
+    height: 280px;
+  }
+
+  @media ${myTheme.media.mobile} {
+    position: static;
+    flex-direction: column-reverse;
+    padding: 30px;
+    gap:30px;
+    height: 100%;
+    align-items: center;
   }
 
   p {
+    text-transform: uppercase;
     font-weight: 400;
     font-size: calc((100vw - 360px) /  (1440 - 360) * (16 - 14) + 14px);
   }
@@ -52,6 +64,10 @@ const StyledDescription = styled.div`
   a {
     font-weight: 500;
     font-size: calc((100vw - 360px) /  (1440 - 360) * (20 - 16) + 16px);
+
+    @media ${myTheme.media.mobile} {
+    text-align:right;
+  }
 }
 
 `;
@@ -63,13 +79,44 @@ const StyledImg = styled.img`
   top: -45px;
   right: 50px;
   border-radius: 20px;
-  width: 60%;
+  width: 50%;
+
+  @media ${myTheme.media.tablet} {
+    top: -35px;
+  right: 30px;
+  }
+
+  @media ${myTheme.media.mobile} {
+    position: static;
+    width: 100%;
+  }
+
 `;
 
 const StyledTextDescription = styled.h3`
   margin: 35px 0 70px 0;
+
+
+  @media ${myTheme.media.tablet} {
+    margin: 25px 0 30px 0;
+    font-size: 20px;
+    line-height: 25px;
+  }
+
+  @media ${myTheme.media.mobile} {
+    margin: 15px 0 20px 0;
+    font-size: 25px;
+    text-align:center;
+  }
 `;
 
 const ServiceInfo = styled.div`
   width: 40%;
+
+  @media ${myTheme.media.mobile} {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
 `;
