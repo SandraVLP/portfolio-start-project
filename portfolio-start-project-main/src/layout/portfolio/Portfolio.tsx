2e2e4858e5dcc733033ff414b3159065bfbb169d
SandraVLP <aleksanvp@yandex.ru>
2024-02-13 17:41:08 +0300

    portfolio adaptive

--- portfolio-start-project-main/src/layout/portfolio/Portfolio.tsx
+++ portfolio-start-project-main/src/layout/portfolio/Portfolio.tsx
@@ -9,7 +9,6 @@ import { FlexWrapper } from "../../components/FlexWrapper";
 import { MyTitle } from "../../components/MyTitle";
 import { Container } from "../../components/Container";
 
-
 export function Portfolio() {
   return (
     <section id="works" className="Portfolio">
@@ -17,25 +16,31 @@ export function Portfolio() {
         <FlexWrapper direction="column">
           <MyTitle text="My recent works" />
           <StyledList>
-            <li><Link href="#all">All</Link></li>
-            <li><Link href="#uxdes" >UX/UI Design</Link></li>
-            <li><Link href="#webdes">Web Design</Link></li>
-            <li><Link href="#frontend">Front End Development</Link></li>
+            <li>
+              <Link href="#all">All</Link>
+            </li>
+            <li>
+              <Link href="#uxdes">UX/UI Design</Link>
+            </li>
+            <li>
+              <Link href="#webdes">Web Design</Link>
+            </li>
+            <li>
+              <Link href="#frontend">Front End Development</Link>
+            </li>
           </StyledList>
           <FlexWrapper
-            wrap="wrap"
-            margin="95px auto"
+          wrap="wrap"
             justify="center"
             gap="25px"
+            padding="0 0 95px 0"
           >
             <StyledImg src={Fitness} alt="Fitness"></StyledImg>
             <StyledImg src={Sight} alt="Sight"></StyledImg>
             <StyledImg src={Puma} alt="Puma"></StyledImg>
             <StyledImg src={Yoga} alt="Yoga"></StyledImg>
           </FlexWrapper>
-          <StyledButton color={myTheme.colors.white}>
-            See more
-          </StyledButton>
+          <StyledButton color={myTheme.colors.white}>See more</StyledButton>
         </FlexWrapper>
       </Container>
     </section>
@@ -45,30 +50,41 @@ export function Portfolio() {
 const StyledList = styled.ul`
   li {
     margin-right: 40px;
+
+    &:last-child{
+      margin-right: 0;
+
+    }
   }
 
+  margin-bottom: 95px;
 
-
-  
-`;
-const Link = styled.a `
-font-weight: 500;
-font-size: calc((100vw - 360px) /  (1440 - 360) * (18 - 14) + 14px);
-
-
-    &:hover {
-      text-decoration: underline;
-    color: ${myTheme.colors.primary};
-    }
+  @media ${myTheme.media.tablet} {
+    margin-bottom: 40px;
     
+  }
+`;
+const Link = styled.a`
+  font-weight: 500;
+  font-size: calc((100vw - 360px) / (1440 - 360) * (18 - 14) + 14px);
 
-  
-`
+  &:hover {
+    text-decoration: underline;
+    color: ${myTheme.colors.primary};
+  }
+`;
 
 const StyledImg = styled.img`
-  width: 50%;
+width: 50%;
   max-width: 545px;
   max-height: 350px;
   border: solid 1px ${myTheme.colors.grey};
   border-radius: 36px;
+
+
+  @media ${myTheme.media.tablet} {
+
+    width: 100%;
+    
+  }
 `;
