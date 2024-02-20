bd4e1be3eaadeb06bec563e514e00af9fd030a72
SandraVLP <aleksanvp@yandex.ru>
2024-02-20 15:03:29 +0300

    burgerbutton usestate

--- portfolio-start-project-main/src/layout/header/mobileMenu/MobileMenu.tsx
+++ portfolio-start-project-main/src/layout/header/mobileMenu/MobileMenu.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { S } from "./MobileMenu_styles";
 import { MenuList } from "../menu/Menu";
 
 export const MobileMenu: React.FC = () => {
+
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const onBurgerButtonClick = () => {setMenuIsOpen(!menuIsOpen)
+
+  }
   return (
     <S.Menu>
-      <S.BurgerButton isOpen={true}>
+      <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerButtonClick}>
         <span></span>
       </S.BurgerButton>
-      <S.MobileMenuPopup isOpen={false}>
+      <S.MobileMenuPopup isOpen={menuIsOpen}>
         <MenuList />
       </S.MobileMenuPopup>
     </S.Menu>
